438b30fac15b06984905dc1f9d2d8e269f3756be
istvan-tamas <istvan.tamas90@gmail.com>
2022-07-03 21:04:05 +0200

    Added food list item

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from typing import List
 
 # main windows obj
 app = Tk()
@@ -21,6 +22,17 @@ calorie_label = Label(app, text='Calorie', font=('Bold', 14), pady=20, padx=10)
 calorie_label.grid(row=0, column=2, sticky=W)
 calorie_entry = Entry(app,textvariable=calorie)
 calorie_entry.grid(row=0, column=3)
+
+# food list
+food_list = Listbox(app, height=8, width=50)
+food_list.grid(row=1, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
+
+food_scroll_bar = Scrollbar(app)
+food_scroll_bar.grid(row=1, column=4)
+# attach scrollbar
+
+food_list.configure(yscrollcommand=food_scroll_bar.set)
+food_scroll_bar.configure(command=food_list.yview)
 
 # calling the main window
 
